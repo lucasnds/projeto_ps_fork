80fb8c334c2100ef7ac2091593f88a06b8f5677e
Lucas Nascimento <lucasdoismile16@gmail.com>
2022-11-30 15:28:17 -0300

    commit

--- public/src/js/informacoes-investimento.js
+++ public/src/js/informacoes-investimento.js
@@ -3,6 +3,7 @@ const iterableInputs = [...inputs]
 
 const tipoInvestimento = localStorage.getItem('tipo_investimento')
 iterableInputs[0].value = tipoInvestimento
+console.log(tipoInvestimento)
 
 
 const aporteInicial = Number(localStorage.getItem('aporte_inicial'))
@@ -10,7 +11,6 @@ const aporteMensal = Number(localStorage.getItem('aporte_mensal'))
 const duracaoInvestimento = Number(localStorage.getItem('duracao_investimento'))
 const valorTotalInvestido = aporteInicial + (aporteMensal * duracaoInvestimento)
 
-console.log(aporteInicial, aporteMensal, duracaoInvestimento)
 iterableInputs[1].value = valorTotalInvestido
 
 iterableInputs[2].value = duracaoInvestimento
@@ -38,20 +38,26 @@ const taxa = Math.fround(localStorage.getItem('taxa_de_juros')) / 100
 const taxaMensal = taxa / 12
 
 let valorTotal = aporteInicial
-let valorTotalSemIR = aporteInicial
+let valorTotalComIR = aporteInicial
 for (let i = 1; i <= duracaoInvestimento; i++) {
     valorTotal += aporteMensal
-    valorTotalSemIR += aporteMensal
+    valorTotalComIR += aporteMensal
     valorTotal += (valorTotal * taxaMensal) * aliquota
-    valorTotalSemIR += valorTotal * taxaMensal
+    valorTotalComIR += valorTotal * taxaMensal
 }
 
-iterableInputs[3].value = valorTotal.toFixed(2)
-iterableInputs[4].value = valorTotalSemIR.toFixed(2)
+iterableInputs[3].value = valorTotalComIR.toFixed(2)
+iterableInputs[4].value = valorTotal.toFixed(2)
 const valorTotalSemInvestimento = aporteInicial + (aporteMensal * duracaoInvestimento)
-iterableInputs[5].value = (valorTotalSemIR - valorTotalSemInvestimento).toFixed(2)
-iterableInputs[6].value = (valorTotalSemIR - valorTotal).toFixed(2)
-iterableInputs[7].value = aliquota * 100 + "%"
+iterableInputs[5].value = (valorTotalComIR - valorTotalSemInvestimento).toFixed(2)
+if (tipoInvestimento === "LCI" || "LCA") {
+    iterableInputs[6].value = "-";
+    iterableInputs[7].value = "-";
+} else if (tipoInvestimento === "Tesouro Direto") {
+    iterableInputs[6].value = ((valorTotalComIR - valorTotalSemInvestimento) * aliquota).toFixed(2)
+    iterableInputs[7].value = aliquota * 100 + "%"
+}
+
 
 iterableInputs[1].style.color = '#000'
 iterableInputs[2].style.color = '#000'
@@ -61,8 +67,8 @@ iterableInputs[5].style.color = '#00982B'
 iterableInputs[6].style.color = '#FF0000'
 iterableInputs[7].style.color = '#FF0000'
 
-const labelInvest = document.getElementById('label-line-invest')
-const labelPoupanca = document.getElementById('label-line-poupanca')
+// const labelInvest = document.getElementById('label-line-invest')
+// const labelPoupanca = document.getElementById('label-line-poupanca')
 
-labelInvest.innerHTML = valorTotal.toFixed(0)
-labelPoupanca.innerHTML = (valorTotal * 0.7).toFixed(0)
+// labelInvest.innerHTML = valorTotal.toFixed(0)
+// labelPoupanca.innerHTML = (valorTotal * 0.7).toFixed(0)
